a7042040a95c6383da017b850dac5c4b85ded6cd
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-09-16 13:08:51 +0530

    heading for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App" data-testid="App.Container"></div>;
+  return (
+    <div className="App" data-testid="App.Container">
+      <h1>Heading</h1>
+    </div>
+  );
 }
 
 export default App;
