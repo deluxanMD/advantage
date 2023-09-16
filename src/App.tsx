3f4b8f233fec667f94e80974b87ca449928db7da
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-09-16 17:58:59 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-
 const App = () => {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <div className="App" data-testid="App.Container">
-      {count > 0 && <h1>Heading</h1>}
-      <button onClick={() => setCount((oldValue) => oldValue + 1)}>
-        Increment
-      </button>
+      <h1>Heading</h1>
     </div>
   );
 };
