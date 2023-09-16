4e02c909c8643b104f6fd7d376e9e527eeeb8193
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-09-16 18:18:24 +0530

    mui configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@mui/material";
+import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
+
 const App = () => {
   return (
     <div className="App" data-testid="App.Container">
       <h1>Headings</h1>
+      <Button variant="contained" color="primary">
+        Primary Button
+      </Button>
+      <AccessAlarm />
+      <ThreeDRotation />
     </div>
   );
 };
