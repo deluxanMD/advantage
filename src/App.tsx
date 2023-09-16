2be73c3eb1e585f6a98a3f3ea2f86718116bcb1f
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-09-16 15:02:11 +0530

    just for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
   return (
     <div className="App" data-testid="App.Container">
       {count > 0 && <h1>Heading</h1>}
+      <button onClick={() => setCount((oldValue) => oldValue + 1)}>
+        Increment
+      </button>
     </div>
   );
 };
